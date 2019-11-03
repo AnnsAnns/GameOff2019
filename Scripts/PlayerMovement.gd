59a1f9d27147f71ecf73c52172c9f54328cc5570
tomGER <tumger@outlook.de>
2019-11-03 20:30:26 +0100

    Dashing is now semi implemented

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,21 +1,35 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
-
+var speed
+var is_dashing = false
 var velocity = Vector2()
 
 func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('right'):
-        velocity.x += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+	speed = 200
+	velocity = Vector2()	
+	
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+		
+	if Input.is_action_pressed("dash") and !is_dashing:
+		dash()
+	
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	get_input()
+	velocity = move_and_slide(velocity)
+
+func dash():
+	speed = 10000
+	is_dashing = true
+	$dash_timer.start()
+
+func _on_dash_timer_timeout():
+	is_dashing = false
