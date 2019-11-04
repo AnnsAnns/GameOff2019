cbe8f792b926497085a023e77dd4fc6c016bfccd
David <dkanekanian1@gmail.com>
2019-11-04 19:23:29 +0000

    Add main menu script

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_Start_pressed():
+	get_tree().change_scene("res://Scenes/maps/test.tscn")
+
+
+func _on_Button_Options_pressed():
+	pass # Replace with function body.
+
+
+func _on_Button_Credits_pressed():
+	pass # Replace with function body.
+
+
+func _on_Button_Quit_pressed():
+	get_tree().quit()
