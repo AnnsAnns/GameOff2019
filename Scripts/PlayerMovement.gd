f1d239bd8d99e8aac2c9698932fe42834a7e662c
tomGER <tumger@outlook.de>
2019-11-17 19:17:55 +0100

    Dash_onto_wall detection and other changes

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,14 +1,19 @@
 extends KinematicBody2D
 
+signal dash_against_wall
+
 var speed
 var is_dashing = false
 var velocity = Vector2()
 
+var normal_speed = 400
+var dash_speed = 10000
+
 func _ready():
 	Global.Player = self
 
-func get_input():
-	speed = 400
+func get_input(delta):
+	speed = normal_speed
 	velocity = Vector2()	
 	
 	if Input.is_action_pressed('right'):
@@ -19,7 +24,7 @@ func get_input():
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
-		
+	
 	if Input.is_action_pressed("dash") and !is_dashing:
 		dash()
 	
@@ -39,17 +44,17 @@ func get_input():
 	else:
 		$AnimatedSprite.animation = "idle"
 	
+	if is_dashing and test_move(transform, velocity * delta):
+		emit_signal("dash_against_wall")
+	
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	velocity = move_and_slide(velocity)
 
 func dash():
-	speed = 10000
+	speed = dash_speed
 	is_dashing = true
 	$dash_timer.start()
 
 func _on_dash_timer_timeout():
 	is_dashing = false
-
-func _on_Barrier_body_entered(body):
-	print("Hit Barrier")
